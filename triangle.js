dfb857dac7c902bc77b4d74d9a82d957360349e0
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-27 14:46:25 +0530

    Add files via upload

---
+++ triangle.js
@@ -0,0 +1,13 @@
+// Area of Circle // Area of  Triangle
+// =============================================
+
+
+
+let base = 25;
+
+let height = 100;
+
+areatriangle = ( base * height ) / 2
+
+console.log('Area of Triangle ', areatriangle)
+
